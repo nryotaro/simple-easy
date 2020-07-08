717f34d311b7d5bba07188cbbb0d7892d29ab96e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-07-08 22:51:42 +0900

    impl Name, TernUp TermDown, Type

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
 module Lib
     ( someFunc
-    ) where
+    )
+where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Name
+    = Const String
+    | Bound Int
+    | Unquoted Int
+    deriving(Show, Eq)
+
+data TermUp 
+    = Ann TermDown Type 
+    | Var Int 
+    | Par Name 
+    | TermUp :@: TermDown
+    deriving(Show, Eq)
+
+data TermDown 
+    = Inf TermUp 
+    | Lam TermDown 
+    deriving(Show, Eq)
+
+data Type 
+    = TPar Name 
+    | Fun Type Type 
+    deriving(Show, Eq)
