5d1a1dce0019eb55ba9960dbed33890d0b94babc
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-07-09 00:29:13 +0900

    Lib

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,19 +12,30 @@ data Name
     | Unquoted Int
     deriving(Show, Eq)
 
-data TermUp 
-    = Ann TermDown Type 
-    | Var Int 
-    | Par Name 
+data TermUp
+    = Ann TermDown Type
+    | Var Int
+    | Par Name
     | TermUp :@: TermDown
     deriving(Show, Eq)
 
-data TermDown 
-    = Inf TermUp 
-    | Lam TermDown 
+data TermDown
+    = Inf TermUp
+    | Lam TermDown
     deriving(Show, Eq)
 
-data Type 
-    = TPar Name 
-    | Fun Type Type 
+data Type
+    = TPar Name
+    | Fun Type Type
     deriving(Show, Eq)
+
+data Value
+    = VLam (Value -> Value)
+    | VNeutral Neutral
+
+data Neutral
+    = NPar Name
+    | NApp Neutral Value
+
+vpar :: Name -> Value
+vpar n = VNeutral (NPar n)
